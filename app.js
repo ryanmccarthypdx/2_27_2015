c2bb06c608997bd35eafabe62dc8533deb1d5bd6
Ryan <ryanmccarthypdx@gmail.com>
2015-02-27 11:21:41 -0800

    Add functionality to beats.reporters.html and correct routes in app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var newspaper = angular.module("newspaper", ["ui.router"]);
 
 newspaper.config(function($stateProvider) {
-  $stateProvider.state("home", {
+  $stateProvider.state("beats", {
     url: "",
     templateUrl: "partials/beats.html",
     controller: "BeatsCtrl"
+  });
+
+  $stateProvider.state("beats.reporters", {
+    url: "/:beatsId",
+    templateUrl: "partials/beats.reporters.html",
+    controller: "ReportersCtrl"
   });
 });
--- services/BeatsFactory.js
+++ services/BeatsFactory.js
@@ -2,7 +2,7 @@ newspaper.factory("BeatsFactory", function BeatsFactory() {
   var factory = {};
   factory.beats = [];
   factory.addBeat = function() {
-    factory.beats.push({ name: factory.beatName, id: (factory.beats.length - 1), reporters: [] });
+    factory.beats.push({ name: factory.beatName, id: (factory.beats.length + 1), reporters: [] });
     factory.beatName = null;
   };
 
