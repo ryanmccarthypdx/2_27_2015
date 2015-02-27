db7b7d4d6873a092d089a46b22ce8c8f1e310073
Ryan <ryanmccarthypdx@gmail.com>
2015-02-27 12:30:54 -0800

    Add ability to add reporters and check their birthday & terms of service

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,6 +1,9 @@
 newspaper.controller("ReportersCtrl", function ReportersCtrl($scope, $stateParams, UtilitiesFactory, BeatsFactory) {
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatsId)
   $scope.addReporter = function() {
-    $scope.beat.reporter.push({ name: $scope.reporterName, birthday: $scope.reporterBday, term: $scope.reporterTerm});
+    $scope.beat.reporters.push({ name: $scope.reporterName, birthday: $scope.reporterBday, term: $scope.reporterTerm, expand: false });
+    $scope.reporterName = null;
+    $scope.reporterBday = null;
+    $scope.reporterTerm = null;
   }
 })
